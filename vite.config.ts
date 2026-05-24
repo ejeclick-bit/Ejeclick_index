cb4df7e7f5572636ba58fc187735e3fcaf6324e9
migu2004 <mi.zambrano21@ciaf.edu.co>
2026-05-24 13:14:47 -0500

    chore: ajustar chunkSizeWarningLimit, tests pasan

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 300,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id: string) {
